aa99e8f691cb4b88b6420ae1dcec7b931b44ccc3
xynazog <ankitbhagat20101994@gmail.com>
2016-02-05 20:08:07 +0530

    Redirection done. First tutorial finished yo. xD

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Rango says hey hello world!")
+	return HttpResponse("Rango says hey hello world!  <br/> <a href='/rango/about'>About</a>")
+
+def abt(request):
+	return HttpResponse("Rango says this is the about page. <br/> <a href='/rango/'>Go back to Main Page!</a>")
